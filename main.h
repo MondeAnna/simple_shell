5f8d2a6c7fa08b8a36439a8482c108c3e28535dd
MondeAnna <monde.anna@gmail.com>
2023-12-13 08:48:07 +0200

    add `main` to `main.h`

--- main.h
+++ main.h
@@ -13,6 +13,8 @@
 #define NULL_BYTE 1
 #define TRUE 1
 
+int main(int argc, char **args, char **environ);
+
 pid_t cmd_exec(char **input, char **environ);
 char *cmd_get(void);
 char **cmd_split(char *cmd);
